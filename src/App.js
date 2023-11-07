e60146098083f8df97743d890c7fb785f665d7a4
Irene Gitau <irene.gitau@strathmore.edu>
2023-11-07 12:38:55 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import QuoteList from './ routes/QuotesLIst'; 
-import AuthorList from './ routes/ AuthorList'; 
+import QuoteList from './ routes/QuotesLIst';
+import AuthorList from './ routes/ AuthorList';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from 'http://localhost:3001' or your API endpoint
+    fetch('http://localhost:3001')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/quotes" element={<QuoteList />} />
-          <Route path="/authors" element={<AuthorList />} />
+          <Route path="/quotes" element={<QuoteList data={data} />} />
+          <Route path="/authors" element={<AuthorList data={data} />} />
         </Routes>
       </div>
     </Router>
