3d4b4dea72d27794048bf4d7ba5c05b8a0111cea
Irene Gitau <irene.gitau@strathmore.edu>
2023-11-07 12:45:19 +0300

    done

--- src/ routes/ AuthorList.js
+++ src/ routes/ AuthorList.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Author from '../components/Author';
 import Button from '../components/Button';
 
 const AuthorList = () => {
-  // Fetch or generate author data here
-  const authors = []; // Your authors data goes here
+  // State to store the authors data
+  const [authors, setAuthors] = useState([]);
+
+  // Function to fetch author data from an API
+  const fetchAuthorsData = async () => {
+    try {
+      const response = await fetch('http://localhost:3001'); // Replace with your API endpoint
+      if (response.ok) {
+        const data = await response.json();
+        setAuthors(data);
+      } else {
+        console.error('Failed to fetch authors data');
+      }
+    } catch (error) {
+      console.error('Error fetching authors data:', error);
+    }
+  };
+
+  // Fetch data when the component mounts
+  useEffect(() => {
+    fetchAuthorsData();
+  }, []);
 
   return (
     <div>
