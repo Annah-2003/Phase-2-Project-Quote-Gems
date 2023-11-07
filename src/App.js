11a056d5aec3db5fd2613ef85624dd228c962bef
Irene Gitau <irene.gitau@strathmore.edu>
2023-11-07 12:13:38 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import QuoteList from './routes/QuoteList';
+import AuthorList from './routes/AuthorList';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/quotes" component={QuoteList} />
+            <Route path="/authors" component={AuthorList} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
