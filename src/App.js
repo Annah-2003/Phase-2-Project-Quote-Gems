cbf88cff3ff04e25813adb76f94f257f4c015de0
Annah-2003 <irene.gitau@strathmore.edu>
2024-02-05 23:55:58 -0800

    done

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   const [quoteData, setQuoteData] = useState(null);
   const [favorites, setFavorites] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
   // Function fetches a random quote from the quotable API
   const fetchRandomQuote = () => {
@@ -25,6 +26,7 @@ function App() {
         console.log(data);
         if (data.content && data.author) {
           setQuoteData({ quote: data.content, author: data.author });
+          setDataLoaded(true);
         } else {
           console.error('No quotes available in the data.');
         }
@@ -33,11 +35,18 @@ function App() {
       .finally(() => setLoading(false));
   };
 
-  // Adds the current quote to the list of favorites
+  // Function to check if a quote already exists in favorites
+  const isQuoteInFavorites = (quote) => {
+    return favorites.some((favorite) => favorite.quote === quote);
+  };
+
+  // Adds the current quote to the list of favorites if it's not already there
   const addToFavorites = () => {
-    if (quoteData) {
+    if (quoteData && !isQuoteInFavorites(quoteData.quote)) {
       setFavorites([...favorites, quoteData]);
       console.log('Quote added to favorites:', quoteData);
+    } else {
+      console.log('Quote already exists in favorites:', quoteData);
     }
   };
 
@@ -51,20 +60,25 @@ function App() {
     <div className="App">
       <Header />
       {/* Display quote-related components if a quote is available */}
-      {quoteData && (
+      {quoteData && dataLoaded && (
         <>
           <Quotes quote={quoteData.quote} />
           <Author author={quoteData.author} />
-          <Button onClick={fetchRandomQuote} label="Get Another Quote" loading={loading} />
+          <div className="button-container">
+            <Button onClick={fetchRandomQuote} label="Get Another Quote" loading={loading} />
+            {/* Display loading spinner if data is being loaded */}
+            {loading && (
+              <div className="loading-spinner"></div>
+            )}
+          </div>
           <FavoriteButton onClick={addToFavorites} disabled={loading} />
-          {loading ? <p>Loading...</p> : null}
+          {loading ? <p>Loading..</p> : null}
           <FavoritesList favorites={favorites} />
         </>
       )}
       <Footer />
     </div>
   );
-  
 }
 
 // Exports App component
